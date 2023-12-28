0c22fa6c3755903dc01762ccc63b8a9feacdfeea
Moses <moses.m917@gmail.com>
2023-12-28 16:34:04 +0200

    Extra javascript

--- style.js
+++ style.js
@@ -1,30 +1,61 @@
 const City = document.querySelector("#input");
 const searchButoon = document.querySelector(".search-btn");
 const locationBtn = document.querySelector("#location_btn");
+const WeatherContainer = document.querySelector(".days");
+const Currentweather = document.querySelector(".card");
 
 
 const API_KEY = "ae510149fcd8a8f4b834a36f2eb53906";
 
-const createWeatherCard =(weatherItem) =>{
-  return ``;
+const createWeatherCard = (cityName, weatherItem, index) => {
+  if (index===0) {
+    return `  <div class="card center-center">
+                <h4>${cityName} ${weatherItem.dt_txt.split(" ")[0]}</h4>
+                <img src="${weatherItem.weather[0].icon}">
+                <P>
+                    <center> <b><small>temp  ${(weatherItem.main.temp -273.15).toFixed(2)}&#8451;</small></b></center>
+                </P>
+            </div>`
+    
+  } else{
+    return ` <div class="day"><b><small>${weatherItem.dt_txt.split(" ")[0]}</small></b> <br>
+            <img src="${weatherItem.weather[0].icon}">
+            <P>
+                    <center> <b><small>temp ${(weatherItem.main.temp -273.15).toFixed(2)}&#8451;</small></b></center>
+            </P>
+        </div>`;
+  }
+
+  
 }
 
 const WeatherDetails = (cityName, lat, lon) => {
   const weather_API_URL = `http://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
   fetch(weather_API_URL).then(res => res.json()).then(data => {
-    const uniqueForecastDays = [];
-    
-    const days = data.list.filter(forecast => {
-        const forecastDate = new Date(forecast.dt_txt).getDate();
-        if (!uniqueForecastDays.includes(forecastDate)) {
-          return uniqueForecastDays.push(forecastDate);
-        }
-    });
 
-    days.forEach(weatherItem  => {
-      createWeatherCard(weatherItem);
-      
+    const uniqueForecastDays = [];
+    const days = data.list.filter(forecast => {
+      const forecastDate = new Date(forecast.dt_txt).getDate();
+      if (!uniqueForecastDays.includes(forecastDate)) {
+        return uniqueForecastDays.push(forecastDate);
+      }
     });
+  
+
+    City.value="";
+    WeatherContainer.innerHTML="";
+
+    console.log(days)
+    days.forEach(weatherItem, index => {
+      if(index===0){
+        WeatherContainer.insertAdjacentElement("beforeend",  createWeatherCard(cityName, weatherItem, index));
+      } else{
+        WeatherContainer.insertAdjacentElement("beforeend",  createWeatherCard(cityName, weatherItem, index));
+      }
+     
+
+    });
+   
 
   }).catch(() => {
     alert("Error! cannot get your weather forcast")
@@ -39,7 +70,7 @@ const getCoordinates = () => {
 
   fetch(API_URL).then(res => res.json()).then(data => {
     if (!data.length) return alert(`cordinate for ${cityName} cannot be found`);
-    console.log(data)
+    // console.log(data)
     const { name, lat, lon } = data[0];
     WeatherDetails(name, lat, lon);
   }).catch(() => {
